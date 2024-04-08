68b9b93fb94cec84f1122c3313cb3437c1b743ac
miya <>
2024-04-08 12:43:58 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yuki/miyamoto3/apiapp/ApiResponse.kt
@@ -0,0 +1,49 @@
+package jp.techacademy.yuki.miyamoto3.apiapp
+import com.squareup.moshi.JsonClass
+
+import com.squareup.moshi.Json
+
+
+@JsonClass(generateAdapter = true)
+data class ApiResponse(
+    @Json(name = "results")
+    val results: Results
+)
+
+@JsonClass(generateAdapter = true)
+data class Results(
+    @Json(name = "shop")
+    val shop: List<Shop>
+)
+
+@JsonClass(generateAdapter = true)
+data class Shop(
+    @Json(name = "address")
+    val address: String,
+    @Json(name = "coupon_urls")
+    val couponUrls: CouponUrls,
+    @Json(name = "id")
+    val id: String,
+    @Json(name = "logo_image")
+    val logoImage: String,
+    @Json(name = "name")
+    val name: String,
+)
+
+@JsonClass(generateAdapter = true)
+data class Budget(
+    @Json(name = "average")
+    val average: String,
+    @Json(name = "code")
+    val code: String,
+    @Json(name = "name")
+    val name: String
+)
+
+@JsonClass(generateAdapter = true)
+data class CouponUrls(
+    @Json(name = "pc")
+    val pc: String,
+    @Json(name = "sp")
+    val sp: String
+)
